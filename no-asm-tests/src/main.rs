36f04c52593efce49df4af16aea1445425797e39
William J <william@mobilecoin.com>
2022-05-13 11:49:09 -0400

    Removing additional eviction for path oram, and making test ignore results with insufficient data

--- no-asm-tests/src/main.rs
+++ no-asm-tests/src/main.rs
@@ -162,7 +162,7 @@ mod tests {
     use mc_oblivious_traits::{rng_maker, testing, HeapORAMStorageCreator, ORAMCreator};
     use std::vec;
     use test_helper::{run_with_one_seed, run_with_several_seeds};
-    const NUMBER_OF_BRANCHES_TO_EVICT: usize = 1;
+    const NUMBER_OF_BRANCHES_TO_EVICT: usize = 0;
     // Run the exercise oram tests for 200,000 rounds in 131072 sized z4 oram
     #[test]
     fn exercise_path_oram_z4_131072() {
@@ -343,14 +343,16 @@ mod tests {
                         dbg!(stash_num, _oram_power);
                     }
                 }
-                let data_variance = rgsl::statistics::variance(
-                    &probability_of_stash_size,
-                    1,
-                    probability_of_stash_size.len(),
-                );
-                #[cfg(debug_assertions)]
-                dbg!(stash_num, data_variance);
-                assert!(data_variance < VARIANCE_THRESHOLD);
+                if probability_of_stash_size.len() > 5 {
+                    let data_variance = rgsl::statistics::variance(
+                        &probability_of_stash_size,
+                        1,
+                        probability_of_stash_size.len(),
+                    );
+                    #[cfg(debug_assertions)]
+                    dbg!(stash_num, data_variance);
+                    assert!(data_variance < VARIANCE_THRESHOLD);
+                }
             }
         });
     }
