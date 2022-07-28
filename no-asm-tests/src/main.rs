832f1940d49b78032c55ac96b6793d3a02434d54
William J <william@mobilecoin.com>
2022-07-28 12:57:08 -0400

    Clean up lint in no asm

--- no-asm-tests/src/main.rs
+++ no-asm-tests/src/main.rs
@@ -48,7 +48,7 @@ where
         }
     }
 
-    return None;
+    None
 }
 
 pub fn main() {
@@ -121,7 +121,7 @@ mod tests {
     use core::convert::TryInto;
     use mc_oblivious_traits::{rng_maker, testing, HeapORAMStorageCreator, ORAMCreator};
     use std::vec;
-    use test_helper::{run_with_several_seeds, run_with_one_seed};
+    use test_helper::{run_with_one_seed, run_with_several_seeds};
 
     // Run the exercise oram tests for 200,000 rounds in 131072 sized z4 oram
     #[test]
@@ -211,7 +211,8 @@ mod tests {
         const VARIANCE_THRESHOLD: f64 = 0.15;
 
         run_with_one_seed(|rng| {
-            let mut oram_size_to_stash_size_by_count = BTreeMap::<u32, BTreeMap<usize, usize>>::default();
+            let mut oram_size_to_stash_size_by_count =
+                BTreeMap::<u32, BTreeMap<usize, usize>>::default();
             let mut maker = rng_maker(rng);
             for oram_power in (10..24).step_by(2) {
                 let mut rng = maker();
@@ -229,7 +230,7 @@ mod tests {
             }
             for stash_num in 1..6 {
                 let mut probability_of_stash_size = vec::Vec::new();
-                for (_oram_power, stash_size_by_count) in &oram_size_to_stash_size_by_count { 
+                for (_oram_power, stash_size_by_count) in &oram_size_to_stash_size_by_count {
                     if let Some(stash_count) = stash_size_by_count.get(&stash_num) {
                         let stash_count_probability =
                             (NUM_ROUNDS as f64 / *stash_count as f64).log2();
